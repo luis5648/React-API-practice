1a178089c52f28e8abf3e0116c21eb4bab0a3411
Luis Roman <porta5648@gmail.com>
2023-10-08 16:11:39 -0600

    notations updated

--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    
     <DataPlaced />
   </React.StrictMode>
 );
-//put this inside </React.StrictMode>to render the default screen:  <App />
+//put <App /> inside </React.StrictMode>to render the default screen
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
