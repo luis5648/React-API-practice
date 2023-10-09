115df655a8559bd28bdaa22a7f8a50cb8bc9fdf3
Luis Roman <luis.roman@solidigm.com>
2023-10-09 12:24:09 -0600

    Fetch API successfylly

--- src/components/Init.tsx
+++ src/components/Init.tsx
@@ -1,24 +1,26 @@
 import { useEffect, useState } from "react";
 import { getInfo } from "../api/ghibli_api";
-
+import React from "react";
 export function DataPlaced() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
+
+  const [AllInfo, setInfo] = useState([]);
+
   useEffect(() => {
     async function loadInfo() {
       const res = await getInfo();
-      <div>res</div>;
-      console.log(res);
+      setInfo(res.data);
     }
     loadInfo();
   }, []);
 
   return (
     <div>
-      <ul>
-        <li>i</li>
-        <li>i</li>
-        <li>i</li>
+      {AllInfo.map(info =>(
+      <ul key={info.id}>
+          <li>{info.title} <p>{info.description}</p></li>
       </ul>
+        ))}
     </div>
   );
 }
