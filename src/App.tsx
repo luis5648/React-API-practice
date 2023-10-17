eb264afeecf278a9c819eed902fe18db29d06ec4
Luis Roman <luis.roman@solidigm.com>
2023-10-16 19:18:24 -0600

    module Routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+          <Route path="/" element={<FilmList/>} />
+        </Routes>
+    </div>
+
+  );
+}
+/*
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
       </header>
-    </div>
-
-  );
-}
-
+*/
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { DataPlaced } from './components/Init';
+import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App/>
-  </React.StrictMode>
+  </BrowserRouter>
 );
     //<DataPlaced />
 //put <App /> inside </React.StrictMode>to render the default screen
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+/*
+
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+*/
 reportWebVitals();
